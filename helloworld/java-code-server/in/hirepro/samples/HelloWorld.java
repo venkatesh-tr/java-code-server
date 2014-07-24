be46595e1f8dcb5df59dd71a3595d1534c362fea
Venkatesha T R <Venkatesha.Ramaiah@hirepro.in>
2014-07-24 16:33:58 +0530

    Second Java Commit

--- helloworld/java-code-server/in/hirepro/samples/HelloWorld.java
+++ helloworld/java-code-server/in/hirepro/samples/HelloWorld.java
@@ -3,7 +3,6 @@ package in.hirepro.samples;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello Git World");
 	}
-
 }
